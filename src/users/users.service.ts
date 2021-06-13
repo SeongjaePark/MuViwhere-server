8e979aa0ae4e47a5204e11a9d6099efe147dd2e4
SeongjaePark <gndan4@gmail.com>
2021-06-13 19:38:29 +0900

    feat: Add an endpoint for finding a user with corresponding id

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entity/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+
+  public async findOne(id: number) {
+    return await this.usersRepository.findOne({ id: id });
+  }
+}
