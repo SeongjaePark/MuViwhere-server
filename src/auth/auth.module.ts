c3a1fb8df6d98263cbd72fbd75292e1de6045a2c
SeongjaePark <gndan4@gmail.com>
2021-06-16 09:49:54 +0900

    feat: Add an endpoint for login (yet, it returns user instead of JWT)

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,20 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/entity/user.entity';
+import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { LocalStrategy } from './passport/local.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  providers: [AuthService],
+  imports: [
+    forwardRef(() => UsersModule),
+    TypeOrmModule.forFeature([User]),
+    PassportModule,
+  ],
+  providers: [AuthService, LocalStrategy],
+  controllers: [AuthController],
   exports: [AuthService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import {
+  forwardRef,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import * as bcrypt from 'bcrypt';
+import { User } from 'src/users/entity/user.entity';
+import { UsersService } from 'src/users/users.service';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
   private readonly saltRound = 10;
+
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+    @Inject(forwardRef(() => UsersService))
+    private readonly usersService: UsersService,
+  ) {}
+
+  public async validateUser(email: string, password: string): Promise<any> {
+    const user = await this.usersRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new UnauthorizedException(`Email ${email} not found!`);
+    }
+
+    if (!(await bcrypt.compare(password, user.password))) {
+      throw new UnauthorizedException('Password is wrong!');
+    }
+
+    return user;
+  }
 
   public async hashPassword(password: string): Promise<string> {
     return await bcrypt.hash(password, this.saltRound);
